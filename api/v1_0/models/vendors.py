a5b50bb993cfa3aa498364deb57eed29a15e67d1
aghabidareh <aghabidareh@gmail.com>
2025-06-25 15:34:18 +0330

    imports

--- api/v1_0/models/vendors.py
+++ api/v1_0/models/vendors.py
@@ -1,10 +1,17 @@
-class Enumerations:
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, BigInteger
+from sqlalchemy.orm import relationship
+
+from api.database.database import Base
+
+
+class Enumerations(Base):
     pass
 
 
-class Vendors:
+class Vendors(Base):
     pass
 
 
-class VendorInformation:
+class VendorInformation(Base):
     pass
