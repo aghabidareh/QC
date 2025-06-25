460a2d2dde4039ff548a5aa4aa03e62829fc77ff
aghabidareh <aghabidareh@gmail.com>
2025-06-25 17:22:57 +0330

    Vendors

--- api/v1_0/models/vendors.py
+++ api/v1_0/models/vendors.py
@@ -14,12 +14,23 @@ class Enumerations(Base):
     extra = Column(Json)
     status = Column(Boolean)
     created_at = Column(DateTime, default=datetime.now)
-    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
-    deleted_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    updated_at = Column(DateTime, onupdate=datetime.now)
+    deleted_at = Column(DateTime, onupdate=datetime.now)
 
 
 class Vendors(Base):
     __tablename__ = 'vendors'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    vendor_id = Column(Integer)
+    profile_id = Column(Integer, ForeignKey('enumerations.id', ondelete="CASCADE"))
+    working_times = None
+    start_date = Column(DateTime)
+    extra = Column(Json)
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, onupdate=datetime.now)
+    deleted_at = Column(DateTime, onupdate=datetime.now)
+    status = Column(Integer)
 
 
 class VendorInformation(Base):
