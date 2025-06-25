5527fe75d6d544cac88ddca4a5effe3fbb5aa2ef
aghabidareh <aghabidareh@gmail.com>
2025-06-25 09:10:40 +0330

    validate-phone-number-of-vendor

--- api/v1.0/schemas/vnedors.py
+++ api/v1.0/schemas/vnedors.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, field_validator
 from typing import Optional, List
+import re
 
 
 class VendorsBase(BaseModel):
@@ -11,7 +12,14 @@ class VendorsBase(BaseModel):
     @field_validator('phone_number_of_owner')
     @classmethod
     def validate_phone_number(cls, v):
-        pass
+        pattern = re.compile(r"^09\d{9}$")
+        if not re.match(pattern, value):
+            raise ValueError("شماره تلفن معتبر نمی‌باشد! باید با 09 شروع شود و 11 رقم باشد.")
+
+        if not value.isascii() or not value.isdigit():
+            raise ValueError("شماره تلفن باید فقط شامل اعداد انگلیسی (0-9) باشد.")
+
+        return value
 
 class Vendors(BaseModel):
     vendors = List[VendorsBase]
