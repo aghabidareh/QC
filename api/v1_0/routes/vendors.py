43428867ae2cb76651ce2c9ed51966f3ed94b3e0
aghabidareh <aghabidareh@gmail.com>
2025-06-25 09:33:38 +0330

    route-of-getting-all-vendors

--- api/v1_0/routes/vendors.py
+++ api/v1_0/routes/vendors.py
@@ -1,7 +1,14 @@
 from fastapi import APIRouter
 import logging
 
+from api.v1_0.schemas.vnedors import Vendors
+
 vendor_router = APIRouter(prefix="/vendors", tags=["Vendors"])
 logger = logging.getLogger(__name__)
 
-@vendor_router.get("/", response_model=Vendors)
+@vendor_router.get("/", response_model=Vendors,
+                   description="Get all vendors connected to the Quick Commerce in Basalam",
+                   response_description="All vendors connected to the Quick Commerce in Basalam",
+                   status_code=200)
+async def get_vendors():
+    pass
