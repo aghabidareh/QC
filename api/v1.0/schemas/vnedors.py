95372d5dda46415d9e131ce22f15e069dc378191
aghabidareh <aghabidareh@gmail.com>
2025-06-25 09:23:08 +0330

    vendor-read

--- api/v1.0/schemas/vnedors.py
+++ api/v1.0/schemas/vnedors.py
@@ -13,18 +13,6 @@ class VendorsBase(BaseModel):
     the_number_of_products: int
     the_number_of_sold_products: int
 
-    @field_validator('phone_number_of_owner')
-    @classmethod
-    def validate_phone_number(cls, value):
-        pattern = re.compile(r"^09\d{9}$")
-        if not re.match(pattern, value):
-            raise ValueError("شماره تلفن معتبر نمی‌باشد! باید با 09 شروع شود و 11 رقم باشد.")
-
-        if not value.isascii() or not value.isdigit():
-            raise ValueError("شماره تلفن باید فقط شامل اعداد انگلیسی (0-9) باشد.")
-
-        return value
-
     class Config:
         from_attributes = True
 
