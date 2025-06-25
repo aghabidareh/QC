6f5cf9d3bd70af760231357f2e99cb6d8ff1fb9c
aghabidareh <aghabidareh@gmail.com>
2025-06-25 18:04:32 +0330

    debugging

--- api/v1_0/models/vendors.py
+++ api/v1_0/models/vendors.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, BigInteger, ForeignKey, Json, Boolean, DateTime
+from sqlalchemy import Column, Integer, String, BigInteger, ForeignKey, JSON, Boolean, DateTime
 from sqlalchemy.orm import relationship
 
 from api.database.database import Base
