2336a8538ca6ba00f5c2ded9f26816a0958a044a
aghabidareh <aghabidareh@gmail.com>
2025-06-25 17:20:35 +0330

    enumerations

--- api/v1_0/models/vendors.py
+++ api/v1_0/models/vendors.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, BigInteger
+from sqlalchemy import Column, Integer, String, BigInteger, ForeignKey, Json, Boolean, DateTime
 from sqlalchemy.orm import relationship
 
 from api.database.database import Base
@@ -7,6 +7,15 @@ from api.database.database import Base
 
 class Enumerations(Base):
     __tablename__ = 'enumerations'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    parent_id = Column(Integer, ForeignKey('enumerations.id', cascade="all, delete-orphan", ondelete="CASCADE"))
+    text = Column(String)
+    extra = Column(Json)
+    status = Column(Boolean)
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    deleted_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
 
 
 class Vendors(Base):
