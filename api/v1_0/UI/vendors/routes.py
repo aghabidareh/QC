25d2a7bdd25611cc3572562f4cb5fd3be92e850f
aghabidareh <aghabidareh@gmail.com>
2025-06-28 14:07:47 +0330

    rename-route-UI

--- api/v1_0/UI/vendors/routes.py
+++ api/v1_0/UI/vendors/routes.py
@@ -11,7 +11,7 @@ from api.database.database import get_db
 from api.v1_0.UI.vendors.models import VendorInformation
 from api.v1_0.UI.vendors.serializers import Vendors, Vendor, Message, VendorCreate, VendorUpdate
 
-vendor_ui_router = APIRouter(prefix="/vendors", tags=["Vendors UI"])
+vendor_ui_router = APIRouter(prefix="/ui/vendors", tags=["Vendors UI"])
 logger = logging.getLogger(__name__)
 
 
