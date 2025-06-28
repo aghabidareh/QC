e1c2eceb06aacc563276d85cbf8f40437df4273a
aghabidareh <aghabidareh@gmail.com>
2025-06-28 14:03:29 +0330

    the-first-one

--- api/v1_0/main/vendors/serializers.py
+++ api/v1_0/main/vendors/serializers.py
@@ -2,5 +2,8 @@ from pydantic import BaseModel, field_validator
 from typing import Optional, List
 import re
 
+class VendorIdBase(BaseModel):
+    pass 
+
 class VendorIdSerializer(BaseModel):
     pass
