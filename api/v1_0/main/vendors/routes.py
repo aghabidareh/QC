38901afabbfa418d2f51384485d6c271cfc6663b
aghabidareh <aghabidareh@gmail.com>
2025-06-28 14:29:02 +0330

    parameters-of

--- api/v1_0/main/vendors/routes.py
+++ api/v1_0/main/vendors/routes.py
@@ -42,8 +42,11 @@ async def get_all(
 
     return VendorIdSerializer(vendors=vendor_identifier, count=total_count)
 
+
 @vendor_main_router.get('/{city_id}', response_model=VendorIdSingle,
                         description='Get the identifier of vendor by city identifier',
                         status_code=200)
-async def get_single_by_city_id():
+async def get_single_by_city_id(
+        db: AsyncSession = Depends(get_db)
+):
     pass
