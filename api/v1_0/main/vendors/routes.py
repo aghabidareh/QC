7460cc4edb04b1792cd9990175e79a7c0018674c
aghabidareh <aghabidareh@gmail.com>
2025-06-28 14:08:31 +0330

    initial-routes

--- api/v1_0/main/vendors/routes.py
+++ api/v1_0/main/vendors/routes.py
@@ -2,4 +2,7 @@ from fastapi import APIRouter, Query
 import logging
 from typing import List
 
+vendor_main_router = APIRouter(prefix="/main/vendors", tags=["Vendors Main"])
+logger = logging.getLogger(__name__)
+
 
