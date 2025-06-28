ead1e6481a310cc885923c9e9549a10f0f9ced7f
aghabidareh <aghabidareh@gmail.com>
2025-06-28 17:58:25 +0330

    routes

--- api/v1_0/main/profiles/routes.py
+++ api/v1_0/main/profiles/routes.py
@@ -9,9 +9,28 @@ from sqlalchemy.sql import func
 
 from api.database.database import get_db
 from api.v1_0.UI.vendors.models import VendorInformation, Vendors, Enumerations
+from api.v1_0.main.profiles.serializers import Profile, Profiles
 
 
 profile_main_router = APIRouter(prefix="/main/profiles", tags=["Profiles Main"])
 logger = logging.getLogger(__name__)
 
+@profile_main_router.get('/profiles', response_model=Profiles,
+                        description='Get the profiles',
+                        status_code=200)
+async def get_profiles(
+        db: AsyncSession = Depends(get_db),
+):
+    query = select(Enumerations).filter(Enumerations.parent_id==5).filter(Enumerations.status==True)
+    result = await db.execute(query)
+    rows = result.scalars().all()
 
+    profiles = [
+        Profile(
+            id=row.id,
+            title=row.title,
+            extra=row.extra,
+        )
+        for row in rows
+    ]
+    return Profiles(profiles=profiles, count=len(profiles))
