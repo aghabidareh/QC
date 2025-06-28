19574a4e9b4f8a95a7ce94a29c58371213603a75
aghabidareh <aghabidareh@gmail.com>
2025-06-28 14:11:05 +0330

    parameters

--- api/v1_0/main/vendors/routes.py
+++ api/v1_0/main/vendors/routes.py
@@ -2,13 +2,23 @@ from fastapi import APIRouter, Query
 import logging
 from typing import List
 
+from fastapi.params import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from sqlalchemy.sql import func
+
+from api.database.database import get_db
+
 from api.v1_0.main.vendors.serializers import VendorIdSerializer
 
 vendor_main_router = APIRouter(prefix="/main/vendors", tags=["Vendors Main"])
 logger = logging.getLogger(__name__)
 
+
 @vendor_main_router.get('/', response_model=VendorIdSerializer,
                         description='Get the identifier of Vendors',
                         status_code=200)
-def get_all():
+def get_all(
+        db: AsyncSession = Depends(get_db)
+):
     pass
