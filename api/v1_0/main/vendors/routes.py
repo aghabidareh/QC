78afb1403a268c1c3b036ab368038f362b84b7e4
aghabidareh <aghabidareh@gmail.com>
2025-06-28 14:10:06 +0330

    initial-the-first-route

--- api/v1_0/main/vendors/routes.py
+++ api/v1_0/main/vendors/routes.py
@@ -2,7 +2,13 @@ from fastapi import APIRouter, Query
 import logging
 from typing import List
 
+from api.v1_0.main.vendors.serializers import VendorIdSerializer
+
 vendor_main_router = APIRouter(prefix="/main/vendors", tags=["Vendors Main"])
 logger = logging.getLogger(__name__)
 
-
+@vendor_main_router.get('/', response_model=VendorIdSerializer,
+                        description='Get the identifier of Vendors',
+                        status_code=200)
+def get_all():
+    pass
