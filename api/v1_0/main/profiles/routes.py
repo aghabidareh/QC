691837841c409a3c6639e58e4263dc179245898f
aghabidareh <aghabidareh@gmail.com>
2025-06-28 17:58:33 +0330

    routes

--- api/v1_0/main/profiles/routes.py
+++ api/v1_0/main/profiles/routes.py
@@ -1,16 +1,13 @@
-from fastapi import APIRouter, Query, HTTPException
 import logging
-from typing import List, Optional
 
+from fastapi import APIRouter
 from fastapi.params import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from sqlalchemy.sql import func
 
 from api.database.database import get_db
-from api.v1_0.UI.vendors.models import VendorInformation, Vendors, Enumerations
+from api.v1_0.UI.vendors.models import Enumerations
 from api.v1_0.main.profiles.serializers import Profile, Profiles
-
 
 profile_main_router = APIRouter(prefix="/main/profiles", tags=["Profiles Main"])
 logger = logging.getLogger(__name__)
