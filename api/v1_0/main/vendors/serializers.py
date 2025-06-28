8faa4d8b8025198df7cf0f64bdd2bf4212355aa5
aghabidareh <aghabidareh@gmail.com>
2025-06-28 14:27:24 +0330

    rename-serializer

--- api/v1_0/main/vendors/serializers.py
+++ api/v1_0/main/vendors/serializers.py
@@ -2,12 +2,12 @@ from pydantic import BaseModel, field_validator
 from typing import Optional, List
 import re
 
-class VendorIdBase(BaseModel):
+class VendorIdSingle(BaseModel):
     vendor_id: int
 
     class Config:
         from_attributes = True
 
 class VendorIdSerializer(BaseModel):
-    vendors: List[VendorIdBase]
+    vendors: List[VendorIdSingle]
     count: int
