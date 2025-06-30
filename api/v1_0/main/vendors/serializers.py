4d23bc20bb6856c411446758178c038865a90b08
aghabidareh <aghabidareh@gmail.com>
2025-06-30 14:53:17 +0330

    new-logic-and-serializer

--- api/v1_0/main/vendors/serializers.py
+++ api/v1_0/main/vendors/serializers.py
@@ -3,32 +3,29 @@ from typing import Optional, List
 import re
 
 
-class VendorIdSingle(BaseModel):
-    vendor_id: int
-
-    class Config:
-        from_attributes = True
-
-
 class VendorIdSerializer(BaseModel):
-    vendors: List[VendorIdSingle]
+    vendors: List[int]
     count: int
+
 
 class ActiveVendor(BaseModel):
     vendor_id: int
     profile_id: Optional[int]
     profile_name: Optional[str]
     working_time: Optional[List[dict]]
-    extra : Optional[dict]
+    extra: Optional[dict]
+
 
 class ActiveVendors(BaseModel):
     vendors: List[ActiveVendor]
     count: int
 
+
 class Profile(BaseModel):
     id: int
     title: Optional[str]
     extra: Optional[dict]
+
 
 class Profiles(BaseModel):
     profiles: List[Profile]
