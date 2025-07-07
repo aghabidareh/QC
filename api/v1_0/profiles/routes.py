50a817d205506dad424180c651846a50d6a6a906
aghabidareh <aghabidareh@gmail.com>
2025-07-07 11:36:46 +0330

    moving routes and serializers for profiles

--- api/v1_0/profiles/routes.py
+++ api/v1_0/profiles/routes.py
@@ -6,8 +6,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
 from api.database.database import get_db
-from api.v1_0.UI.vendors.models import Enumerations
-from api.v1_0.main.profiles.serializers import Profile, Profiles
+from api.v1_0.vendors.models import Enumerations
+from api.v1_0.profiles.serializers import Profile, Profiles
 
 profile_main_router = APIRouter(prefix="/v1/main/profiles", tags=["Profiles Main"])
 logger = logging.getLogger(__name__)
