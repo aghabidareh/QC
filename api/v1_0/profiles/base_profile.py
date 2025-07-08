f43272bb57b94f41946a5f6e0e762fbf16ed2afa
aghabidareh <aghabidareh@gmail.com>
2025-07-08 13:15:34 +0330

    rename what was wrong

--- api/v1_0/profiles/base_profile.py
+++ api/v1_0/profiles/base_profile.py
@@ -3,7 +3,7 @@ from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
-class BaseVendor:
+class BaseProfile:
     def __init__(self, db: AsyncSession, current_user: Optional[dict] = None):
         self.db = db
         self.current_user = current_user
