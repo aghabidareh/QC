d9d3bfb5933ac71f0b992d6e33fd3f3ad27c844a
aghabidareh <aghabidareh@gmail.com>
2025-07-09 17:54:56 +0330

    create access token utils

--- api/v1_0/accounts/utils.py
+++ api/v1_0/accounts/utils.py
@@ -5,14 +5,15 @@ from fastapi import HTTPException, status, Depends
 from fastapi.security import HTTPBearer
 from jose import JWTError, jwt
 
-
 security = HTTPBearer()
 
 SECRET_KEY = "Sahel-Is-Here"
+ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 365 * 24 * 360 * 360
 
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
-    pass
-
-
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
