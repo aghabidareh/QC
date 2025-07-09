9ac57933fc58a4ec10c9d99d22c1a887ed724f33
aghabidareh <aghabidareh@gmail.com>
2025-07-09 17:56:00 +0330

    get current user utility

--- api/v1_0/accounts/utils.py
+++ api/v1_0/accounts/utils.py
@@ -17,3 +17,7 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
     expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+async def get_current_user(token: str = Depends(security))
+    pass
