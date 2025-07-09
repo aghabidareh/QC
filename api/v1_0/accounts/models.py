e39ea34e12fa17518a324ef3d500458026376fa1
aghabidareh <aghabidareh@gmail.com>
2025-07-09 17:40:43 +0330

    libriries

---
+++ api/v1_0/accounts/models.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, BigInteger, ForeignKey, JSON, Boolean, DateTime
+from sqlalchemy.orm import relationship
+
+from api.database.database import Base
+
+
+
