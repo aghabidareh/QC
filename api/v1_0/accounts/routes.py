dacf650a3cd4e677df0e012ffdd2fe3dc80cf162
aghabidareh <aghabidareh@gmail.com>
2025-07-09 18:08:37 +0330

    the routes

--- api/v1_0/accounts/routes.py
+++ api/v1_0/accounts/routes.py
@@ -10,4 +10,13 @@ from api.v1_0.accounts.utils import create_access_token
 
 accounts_router = APIRouter(prefix="/accounts", tags=["Accounts"])
 
+@accounts_router.post("/signup", response_description="the information of the new user",
+                            status_code=201)
+async def signup(owner: Account, db: AsyncSession = Depends(get_db)):
+    pass
 
+
+@accounts_router.post('/login', response_description="the token for the user",
+                            status_code=200)
+async def login(owner: Account, db: AsyncSession = Depends(get_db)):
+    pass
