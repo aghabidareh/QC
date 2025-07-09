534a3b6293b91df64e40fea7d4f6045e5e7d16a8
aghabidareh <aghabidareh@gmail.com>
2025-07-09 18:07:02 +0330

    the library of the routes

---
+++ api/v1_0/accounts/routes.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+from api.database.database import get_db
+from api.v1_0.accounts.models import Accounts
+from api.v1_0.accounts.serializers import Account
+from api.v1_0.accounts.utils import create_access_token
+
+
+
+
