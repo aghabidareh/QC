52ac04dd11ac0d22a9d5576727abc74d5b70dc9d
aghabidareh <aghabidareh@gmail.com>
2025-07-09 18:12:08 +0330

    the logic of route fo sign up

--- api/v1_0/accounts/routes.py
+++ api/v1_0/accounts/routes.py
@@ -12,11 +12,26 @@ accounts_router = APIRouter(prefix="/accounts", tags=["Accounts"])
 
 @accounts_router.post("/signup", response_description="the information of the new user",
                             status_code=201)
-async def signup(owner: Account, db: AsyncSession = Depends(get_db)):
-    pass
+async def signup(account: Account, db: AsyncSession = Depends(get_db)):
+    stmt = select(Accounts).where(Accounts.phone_number == account.phone_number)
+    result = await db.execute(stmt)
+    existing_owner = result.scalar()
+
+    if existing_owner:
+        raise HTTPException(status_code=400, detail="شماره تلفن وارد شده به صورت پیش فرض وجود دارد.")
+
+    new_account = Accounts(
+        phone_number=account.phone_number,
+        password=account.password
+    )
+    db.add(new_account)
+    await db.commit()
+    await db.refresh(new_account)
+
+    return new_account
 
 
 @accounts_router.post('/login', response_description="the token for the user",
                             status_code=200)
-async def login(owner: Account, db: AsyncSession = Depends(get_db)):
+async def login(account: Account, db: AsyncSession = Depends(get_db)):
     pass
