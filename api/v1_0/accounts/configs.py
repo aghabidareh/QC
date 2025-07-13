9bdd466788e35829ff2868c898113254049d992a
aghabidareh <aghabidareh@gmail.com>
2025-07-13 09:06:24 +0330

    first let's load the configurations

---
+++ api/v1_0/accounts/configs.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+REDIRECT_URI = os.getenv("REDIRECT_URI")
+AUTH_SERVER_URL = os.getenv("AUTH_SERVER_URL")
+TOKEN_URL = os.getenv("TOKEN_URL")
+AUTHORIZE_URL = os.getenv("AUTHORIZE_URL")
+KONG_GATEWAY_URL = os.getenv("KONG_GATEWAY_URL")
