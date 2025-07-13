aad00f7c677823bffba20558d8693796fafd679a
aghabidareh <aghabidareh@gmail.com>
2025-07-13 09:12:46 +0330

    preparing the response after logging in which is named callback params() logic()

--- api/v1_0/accounts/routes.py
+++ api/v1_0/accounts/routes.py
@@ -1,12 +1,13 @@
 from urllib.parse import urlencode
 
+import httpx
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.responses import RedirectResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
 from api.database.database import get_db
-from api.v1_0.accounts.configs import AUTHORIZE_URL, CLIENT_ID, REDIRECT_URI
+from api.v1_0.accounts.configs import AUTHORIZE_URL, CLIENT_ID, REDIRECT_URI, TOKEN_URL, CLIENT_SECRET
 from api.v1_0.accounts.models import Accounts
 from api.v1_0.accounts.serializers import Account
 from api.v1_0.accounts.utils import create_access_token
@@ -31,4 +32,22 @@ async def login():
 
 @account_router.get("/callback")
 async def callback(code: str):
-    pass
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            TOKEN_URL,
+            data={
+                "grant_type": "authorization_code",
+                "code": code,
+                "redirect_uri": REDIRECT_URI,
+                "client_id": CLIENT_ID,
+                "client_secret": CLIENT_SECRET,
+            },
+        )
+        if response.status_code != 200:
+            raise HTTPException(status_code=400, detail="Failed to obtain access token")
+
+        token_data = response.json()
+        access_token = token_data.get("access_token")
+        refresh_token = token_data.get("refresh_token")
+
+        return {"access_token": access_token, "refresh_token": refresh_token}
