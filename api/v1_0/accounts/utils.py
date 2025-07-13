6413e4f65449091ea56cd7c50f2ce4d2571a836b
aghabidareh <aghabidareh@gmail.com>
2025-07-13 10:00:44 +0330

    tenth unnecessary things

--- api/v1_0/accounts/utils.py
+++ api/v1_0/accounts/utils.py
@@ -1,9 +1,14 @@
 from datetime import datetime, timedelta
 
 import jwt
+import httpx
 from fastapi import HTTPException, status, Depends
 from fastapi.security import HTTPBearer
 from jose import JWTError, jwt
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from api.database.database import get_db
+from api.v1_0.accounts.routes import oauth2_scheme, VALIDATE_URL
 
 security = HTTPBearer()
 
@@ -17,18 +22,3 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
     expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-
-
-async def get_current_user(token: str = Depends(security)):
-    credentials_exception = HTTPException(
-        status_code=401,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token.credentials, SECRET_KEY, algorithms=[ALGORITHM])
-        if "sub" not in payload:
-            raise credentials_exception
-        return {"phone_number": payload["sub"]}
-    except JWTError:
-        raise credentials_exception
