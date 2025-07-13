4dac5caaa72b460401f5fa12aa0dcb57485d1177
aghabidareh <aghabidareh@gmail.com>
2025-07-13 10:41:59 +0330

    eleventh configurations

--- api/v1_0/accounts/routes.py
+++ api/v1_0/accounts/routes.py
@@ -1,17 +1,16 @@
+import logging
 from urllib.parse import urlencode
+
 import httpx
-from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.responses import RedirectResponse
 from fastapi.security import OAuth2AuthorizationCodeBearer
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-import logging
-from typing import Dict, List
 
 from api.database.database import get_db
 from api.v1_0.accounts.configs import AUTHORIZE_URL, CLIENT_ID, REDIRECT_URI, TOKEN_URL, CLIENT_SECRET
 from api.v1_0.accounts.models import Accounts
-from api.v1_0.accounts.serializers import Account
 from api.v1_0.accounts.utils import create_access_token
 
 logging.basicConfig(level=logging.INFO)
@@ -25,12 +24,6 @@ oauth2_scheme = OAuth2AuthorizationCodeBearer(
 )
 
 VALIDATE_URL = "https://auth.basalam.com/validate"
-KONG_GATEWAY_URL = "https://api.basalam.com"
-
-REQUIRED_SCOPES = {
-    "/wallet": ["customer.wallet.read"],
-    "/wallet/spend": ["customer.wallet.write"],
-}
 
 
 async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)):
