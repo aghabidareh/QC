72900c908f7f3a6b2a316ded48b50c5d2d28324f
aghabidareh <aghabidareh@gmail.com>
2025-07-13 09:23:18 +0330

    first getting the current user

--- api/v1_0/accounts/routes.py
+++ api/v1_0/accounts/routes.py
@@ -13,11 +13,16 @@ from api.v1_0.accounts.models import Accounts
 from api.v1_0.accounts.serializers import Account
 from api.v1_0.accounts.utils import create_access_token
 
-
 account_router = APIRouter(prefix="/accounts", tags=["Accounts"])
-
 
 oauth2_scheme = OAuth2AuthorizationCodeBearer(
     authorizationUrl=AUTHORIZE_URL,
     tokenUrl=TOKEN_URL,
 )
+
+VALIDATE_URL = "https://auth.basalam.com/validate"
+KONG_GATEWAY_URL = "https://api.basalam.com"
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)):
+    pass
