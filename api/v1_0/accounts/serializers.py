ebaf2121d293c0b5992becded4b6dcddf24e1946
aghabidareh <aghabidareh@gmail.com>
2025-07-13 09:58:45 +0330

    ninth serializer

--- api/v1_0/accounts/serializers.py
+++ api/v1_0/accounts/serializers.py
@@ -2,17 +2,6 @@ from pydantic import BaseModel, field_validator
 import re
 
 class Account(BaseModel):
-    phone_number: str
-    password: str
-
-    @field_validator("phone_number")
-    @classmethod
-    def validate_phone(cls, value):
-        pattern = re.compile(r"^09\d{9}$")
-        if not re.match(pattern, value):
-            raise ValueError("شماره تلفن معتبر نمی‌باشد! باید با 09 شروع شود و 11 رقم باشد.")
-
-        if not value.isascii() or not value.isdigit():
-            raise ValueError("شماره تلفن باید فقط شامل اعداد انگلیسی (0-9) باشد.")
-
-        return value
+    user_id: str
+    access_token: str
+    refresh_token: str | None = None
