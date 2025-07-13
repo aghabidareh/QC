92141b038bfd7bfff9f229c0c05a0aaba95a662b
aghabidareh <aghabidareh@gmail.com>
2025-07-13 09:10:18 +0330

    the configs to logging in basalam

--- api/v1_0/accounts/routes.py
+++ api/v1_0/accounts/routes.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
 from api.database.database import get_db
-from api.v1_0.accounts.configs import AUTHORIZE_URL
+from api.v1_0.accounts.configs import AUTHORIZE_URL, CLIENT_ID, REDIRECT_URI
 from api.v1_0.accounts.models import Accounts
 from api.v1_0.accounts.serializers import Account
 from api.v1_0.accounts.utils import create_access_token
@@ -17,8 +17,13 @@ account_router = APIRouter(prefix="/accounts", tags=["Accounts"])
 
 @account_router.get("/login")
 async def login():
-    scopes = []
-    params = {}
+    scopes = ['vendor.profile.read', 'vendor.profile.write', 'customer.profile.write', 'customer.profile.read', 'vendor.product.read']
+    params = {
+        "client_id": CLIENT_ID,
+        "redirect_uri": REDIRECT_URI,
+        "response_type": "code",
+        "scope": " ".join(scopes),
+    }
 
     auth_url = f"{AUTHORIZE_URL}?{urlencode(params)}"
     return RedirectResponse(url=auth_url)
