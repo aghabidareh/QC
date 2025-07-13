a7aee533282d4119fb96afd8d6542d1852357b34
aghabidareh <aghabidareh@gmail.com>
2025-07-13 09:08:40 +0330

    base of everything of logging into basalam

--- api/v1_0/accounts/routes.py
+++ api/v1_0/accounts/routes.py
@@ -1,8 +1,12 @@
+from urllib.parse import urlencode
+
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.responses import RedirectResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
 from api.database.database import get_db
+from api.v1_0.accounts.configs import AUTHORIZE_URL
 from api.v1_0.accounts.models import Accounts
 from api.v1_0.accounts.serializers import Account
 from api.v1_0.accounts.utils import create_access_token
@@ -13,4 +17,8 @@ account_router = APIRouter(prefix="/accounts", tags=["Accounts"])
 
 @account_router.get("/login")
 async def login():
-    pass
+    scopes = []
+    params = {}
+
+    auth_url = f"{AUTHORIZE_URL}?{urlencode(params)}"
+    return RedirectResponse(url=auth_url)
